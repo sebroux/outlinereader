c480ae2919159fb3542470b4fa6b56a386a6c71c
roux.sebastien@gmail.com <roux.sebastien@gmail.com@1d766212-46de-464e-dc9e-6cce891930d1>
2012-08-16 15:18:21 +0000

    .

--- trunk/OutlineReader/exe.bat
+++ trunk/OutlineReader/exe.bat
@@ -1,11 +1,9 @@
-rem java -jar "C:\Documents and Settings\seb\Mes documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" -u admin -p password -s localhost -a sample_u -d basic -i > output.txt
+@echo off
 
-rem java -jar "C:\Documents and Settings\seb\Mes documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" -u admin -p password -s localhost -a cosstrat -d cosstrat -i -m "fonctions" > output.txt
+java -jar "C:\Users\sroux\Documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" -u admin -p password -s localhost -a sample_u -d basic > output.txt
 
-rem java -jar "C:\Documents and Settings\seb\Mes documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" admin password sr-dev sample basic true > output.txt
+java -jar "C:\Users\sroux\Documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" -h >> output.txt
 
-java -jar "C:\Documents and Settings\seb\Mes documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" -h > output.txt
-
-rem java -jar "C:\Documents and Settings\seb\Mes documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" -u admin -p password -s localhost -a cosstrat -d cosstrat -i -m "fonctions" -v "http://localhost:13080/aps/JAPI"> output.txt
+rem java -jar "C:\Users\sroux\Documents\NetBeansProjects\OutlineReader\dist\OutlineReader.jar" -u admin -p password -s localhost -a sample -d basic -v "http://localhost:13080/aps/JAPI"> output.txt
 
 pause
